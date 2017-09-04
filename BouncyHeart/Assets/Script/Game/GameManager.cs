836cff5779de66d0c906594fcfe50ee66e6f1b96
佐藤秀輔 <sato.fun0619@gmail.com>
2017-09-04 18:00:52 +0900

    テストで作成したものを追加

---
+++ BouncyHeart/Assets/Script/Game/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject Player;
+	public Text gameOverText;
+	private bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			PlayerDamaged (1);
+		}
+		if (gameOver) {
+			gameOverText.enabled = true;
+		}
+	}
+
+	void PlayerDamaged(int damage){
+		setPlayerHP (getPlayerHP () - damage);
+		if (getPlayerHP () <= 0) {
+			// がめおべら
+			Debug.Log ("you died!");
+			gameOver = true;
+		}
+	}
+
+	void PlayerHealed(int heal) {
+		int setHP = System.Math.Min (Player.GetComponent<Player> ().PLAYER_HP_MAX, getPlayerHP () + heal);
+		setPlayerHP (setHP);
+	}
+
+	void setPlayerHP(int playerHP){
+		Player.GetComponent<Player> ().playerHP = playerHP;
+		Debug.Log (playerHP);
+	}
+	int getPlayerHP(){
+		return Player.GetComponent<Player> ().playerHP;
+	}
+}
