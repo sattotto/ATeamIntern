94f75362b74279e13437fc8737edeedba9244c46
佐藤秀輔 <sato.fun0619@gmail.com>
2017-09-09 19:49:31 +0900

    ストップボタンの追加 機能はなく押した際にコンソールに流れるのみ

--- BouncyHeart/Assets/Script/Game/GameSpeedButton.cs
+++ BouncyHeart/Assets/Script/Game/GameSpeedButton.cs
@@ -9,13 +9,13 @@ public class GameSpeedButton : MonoBehaviour {
 
 	public void ButtonPush() {
 		Debug.Log("Button Push !!");
-		speedCount += 1;
-		if (speedCount > Const.SPEED.Length - 1) {
-			speedCount = 0;
-		}
+//		speedCount += 1;
+//		if (speedCount > Const.SPEED.Length - 1) {
+//			speedCount = 0;
+//		}
 		// Textコンポーネント郡を取得します。
-		var components = this.gameObject.GetComponentsInChildren<Text>();
+//		var components = this.gameObject.GetComponentsInChildren<Text>();
 		// テキストを文字の状態によって変更するようにします。
-		components[0].text = "x"+(speedCount+1);
+//		components[0].text = "x"+(speedCount+1);
 	}
 }
