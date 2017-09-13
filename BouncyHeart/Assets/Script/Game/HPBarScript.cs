9f58550d503649bb441fb888c466fa75f25a7cf7
佐藤秀輔 <sato.fun0619@gmail.com>
2017-09-13 19:09:29 +0900

    HPBarの追加

---
+++ BouncyHeart/Assets/Script/Game/HPBarScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarScript : MonoBehaviour {
+
+	Slider _slider;
+	void Start () {
+		// スライダーを取得する
+		_slider = GameObject.Find("Slider").GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// HPゲージに値を設定
+		_slider.value = Player.playerHP;
+	}
+}
