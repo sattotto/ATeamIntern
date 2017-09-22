b40aaa19625a09de6de87fdd2db0f83b6950fd18
Ayaka.Y <ayaka.yamanaka.8@gmail.com>
2017-09-23 05:42:01 +0900

    ホーム画面の追加、タッチした場所にエフェクトが出る

---
+++ BouncyHeart/Assets/Script/Home/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    int charaId;
+	// Use this for initialization
+	void Start () {
+        charaId = Random.Range(0, 4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ BouncyHeart/Assets/Script/Home/PlayTouchPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTouchPanel : MonoBehaviour
+{
+    // 再生したいEffectのプレハブを指定する
+    public GameObject Effect01 = null;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // タッチエフェクトを仮実装
+        if (Input.GetMouseButtonDown(0))
+        {
+            // タッチした画面座標からワールド座標へ変換
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5.0f));
+            // 指定したエフェクトを作成
+            GameObject go = (GameObject)Instantiate(Effect01, pos, Quaternion.identity);
+
+            // エフェクトを消す
+            Destroy(go, 0.4f);
+        }
+    }
+}
