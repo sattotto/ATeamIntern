ae06678101c51eecd1ab2e984d10ad828eda48c0
Ayaka.Y <ayaka.yamanaka.8@gmail.com>
2017-09-23 05:49:09 +0900

    ランダムでキャラが表示

--- BouncyHeart/Assets/Script/Home/Main.cs
+++ BouncyHeart/Assets/Script/Home/Main.cs
@@ -5,13 +5,54 @@ using UnityEngine;
 public class Main : MonoBehaviour {
 
     int charaId;
-	// Use this for initialization
-	void Start () {
+
+    //描画用
+    GameObject[] Chara;
+
+    SpriteRenderer CharaSpriteRenderer;
+
+
+    //きゃらのイラスト一覧
+    public Sprite Kokonoha;
+    public Sprite Fulost;
+    public Sprite Milky;
+    public Sprite King;
+    public Sprite Ai;
+
+    // Use this for initialization
+    void Start () {
+
+        Chara = GameObject.FindGameObjectsWithTag("Charactor");
+        // このobjectのSpriteRendererを取得
+        CharaSpriteRenderer = Chara[0].GetComponent<SpriteRenderer>();
+
         charaId = Random.Range(0, 4);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+        Debug.Log(charaId);
+        switch (charaId)
+        {
+            case 0:
+                CharaSpriteRenderer.sprite = Kokonoha;
+                break;
+            case 1:
+                CharaSpriteRenderer.sprite = Fulost;
+                break;
+            case 2:
+                CharaSpriteRenderer.sprite = Milky;
+                break;
+            case 3:
+                CharaSpriteRenderer.sprite = King;
+                break;
+            case 4:
+                CharaSpriteRenderer.sprite = Ai;
+                break;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
