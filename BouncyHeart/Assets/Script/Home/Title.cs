95081ca8a5690ec63bb19f340b3a718e4b01d2e6
Ayaka.Y <ayaka.yamanaka.8@gmail.com>
2017-09-27 00:49:24 +0900

    タイトルの追加

---
+++ BouncyHeart/Assets/Script/Home/Title.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    private AudioSource clickSe;
+
+    int count = 0;
+    int timer = 0;
+    bool inBattle = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        AudioSource[] audioSource = GetComponents<AudioSource>();
+        clickSe = audioSource[0];
+        inBattle = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            inBattle = true;
+        }
+
+        if (inBattle)
+        {
+            count++;
+            if (count <= 15)
+            {
+                if (timer == 0)
+                {
+                    clickSe.PlayOneShot(clickSe.clip);
+                }
+                timer++;
+            }
+            if (count > 15)
+            {
+                SceneManager.LoadScene("Home");
+
+            }
+        }
+    }
+}
---
+++ BouncyHeart/Assets/Script/Home/TitleText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleText : MonoBehaviour {
+    private float nextTime;
+    public float interval = 0.0001f;    // 点滅周期
+    int time = 0;
+    // Use this for initialization
+    void Start () {
+        nextTime = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        time++;
+        if (time > 45)
+        {
+            GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled;
+            time = 0;
+
+        }
+
+    }
+}
